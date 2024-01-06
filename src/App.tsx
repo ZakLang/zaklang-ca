b7cc7e544a0c844271442ab04d8e8570e4792bc3
Zak Lang <zachary.w.lang@gmail.com>
2024-01-05 16:52:29 -0800

    Reset to basic template project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Body from './Body';
-import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>I'm Zak Lang</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <main>
-
-      </main>
-      <footer>
-
-      </footer>
     </div>
   );
 }
